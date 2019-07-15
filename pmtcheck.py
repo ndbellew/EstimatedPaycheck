0c770bb4739d9698984c4f50deb2b4b403335a2d
ndbellew <superdaggler68@gmail.com>
2019-07-15 11:35:39 -0600

    app created

---
+++ pmtcheck.py
@@ -0,0 +1,5 @@
+# Estimated Paycheck
+# Author: Nathan Bellew
+#
+DISCLAIMER = "pmtcheck.py is is a payment checker based loosely off of equations found on local government sites. It may not be 100% accurate and is only an estimate please be cautious."
+VERSION = "0.1a ALPHA"
